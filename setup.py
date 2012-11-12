b95938b6ae38cf0135d4c478587600c0f18b4cd8
Thomas Bytheway <tb403@cam.ac.uk>
2012-11-12 11:54:38 +0000

    Added basic layout for the setup.py file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='OPUS',
+      version='0.0.1',
+      description='Observational Provenance in User Space',
+      author='Thomas Bytheway, Nikilesh Balakrishnan',
+      author_email='tb403@cam.ac.uk, nb466@cam.ac.uk',
+      url='',
+     )
