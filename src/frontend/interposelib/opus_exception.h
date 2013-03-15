1da3cb00795d41f365ee98ca7138e9aa5874933e
Nikilesh Balakrishnan <nb466@cl.cam.ac.uk>
2013-03-15 11:34:46 +0000

    added opus exceptions class

---
+++ src/frontend/interposelib/opus_exception.h
@@ -0,0 +1,30 @@
+#ifndef SRC_FRONTEND_INTERPOSELIB_OPUS_EXCEPTION_H_
+#define SRC_FRONTEND_INTERPOSELIB_OPUS_EXCEPTION_H_
+
+#include <string>
+#include <stdio.h>
+
+class OPUSException
+{
+    public:
+        OPUSException(const std::string& file,
+                        const int line, const std::string& msg)
+        {
+            file_name = file;
+            line_no = line;
+            message = msg;
+        }
+
+        void print_msg()
+        {
+            DEBUG_LOG("[%s:%d]: %s\n", file_name.c_str(),
+                        line_no, message.c_str());
+        }
+
+    private:
+        int line_no;
+        std::string file_name;
+        std::string message;
+};
+
+#endif  // SRC_FRONTEND_INTERPOSELIB_OPUS_EXCEPTION_H_
