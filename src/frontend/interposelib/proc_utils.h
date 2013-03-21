fe5519cc8e0d0d1d4ef9b6fa4271bcc52b058346
Thomas Bytheway <tb403@cam.ac.uk>
2013-03-21 17:37:37 +0000

    Added message serialisation function to ProcUtils.
    Added a method named serialise_and_send_data to the ProcUtils
    class to allow easy transmission of protocol buffer messages
    from the application.
    
    Signed-off-by: Thomas Bytheway <tb403@cam.ac.uk>

--- src/frontend/interposelib/proc_utils.h
+++ src/frontend/interposelib/proc_utils.h
@@ -1,13 +1,16 @@
 #ifndef SRC_FRONTEND_INTERPOSELIB_PROC_UTILS_H_
 #define SRC_FRONTEND_INTERPOSELIB_PROC_UTILS_H_
 
-#include <string>
+#include "uds_msg.pb.h"
+
+typedef ::google::protobuf::Message Message;
 
 class ProcUtils
 {
     public:
         static uint64_t get_time();
         static bool test_and_set_flag(const bool value);
+        static void serialise_and_send_data(const Message& msg_obj);
 
     private:
         static bool in_func_flag;
