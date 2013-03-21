a257622f32dff37d885d097b0af31b90a04653b8
Nikilesh Balakrishnan <nb466@cl.cam.ac.uk>
2013-03-21 09:52:44 +0000

    1) Code modification to ensure that header and payload messages can be received even if there is a delay between the two.2) Locking before using analyser. 3)Other misc stuff

--- src/backend/opus/common_utils.py
+++ src/backend/opus/common_utils.py
@@ -1,9 +1,78 @@
 '''
-Contains common functions that may be used
-by multiple modules in the OPUS backend
+Contains common classes and functions that can
+be used by multiple modules in the OPUS backend
 '''
-
+from threading import Lock
+import copy
 import uds_msg_pb2
+
+
+class OPUSException(Exception):
+    '''Simple exception handling class'''
+    def __init__(self, msg):
+        '''Initialize message'''
+        super(OPUSException, self).__init__()
+        self.msg = msg
+    def __str__(self):
+        '''Return message'''
+        return self.msg
+
+class InvalidTagException(OPUSException):
+    '''Exception class to handle invalid tags'''
+    def __init__(self, tag):
+        '''Set the message in the base class'''
+        super(InvalidTagException, self).__init__("Invalid tag: %s" % tag)
+
+
+class FixedDict(object):
+    '''Ensures keys are fixed in the dictionary'''
+    def __init__(self, dictionary):
+        '''Take a copy of the dictionary'''
+        super(FixedDict, self).__init__()
+        self._dictionary = copy.deepcopy(dictionary)
+    def __setitem__(self, key, item):
+        '''Sets a value for a valid key'''
+        if key not in self._dictionary:
+            raise KeyError("The key {} is not defined.".format(key))
+        self._dictionary[key] = item
+    def __getitem__(self, key):
+        '''Returns the value for a valid key'''
+        if key not in self._dictionary:
+            raise KeyError("The key {} is not defined.".format(key))
+        return self._dictionary[key]
+
+def meta_factory(base, tag, *args):
+    '''Return an instance of the class 
+    derived from base with the name "tag"'''
+    def compute_subs(cls):
+        '''Compute the transitive colsure of
+        the subclass relation on the given class'''
+        sub_classes = [cls]
+        for sub_class in cls.__subclasses__():
+            sub_classes += compute_subs(sub_class)
+        return sub_classes
+
+    sub_classes = compute_subs(base)
+    for sub_class in sub_classes:
+        if sub_class.__name__ == tag:
+            return sub_class(*args)
+    raise InvalidTagException(tag)
+
+def enum(**enums):
+    '''Returns an enum class object'''
+    return type('Enum', (), enums)
+
+
+def analyser_lock(func):
+    '''Decorator method for accessing analyser object'''
+    if not hasattr(analyser_lock, "mutex"):
+        analyser_lock.mutex = Lock()
+    def deco(self, *args, **kwargs):
+        '''Wraps function call with lock acquire and release'''
+        with analyser_lock.mutex:
+            return func(self, *args, **kwargs)
+    return deco
+
 
 def header_size():
     '''Creates a header object and returns the object size'''
--- src/backend/opus/production.py
+++ src/backend/opus/production.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python -tt
+
 '''
 This module contains the implementation of
 various types of producer classes. It also
@@ -10,78 +12,60 @@ import os
 import sys
 import struct
 import time
+import errno
 import threading
 import socket
+from socket import SOL_SOCKET
 import select
+import logging
 import uds_msg_pb2
 import common_utils
 import analysis
-from socket import SOL_SOCKET
 
-
-def create_close_conn_obj(sock_fd):
-    '''Returns objects to mark a client connection close'''
-    return None, None
-
+def unlink_uds_path(path):
+    '''Remove UDS link'''
+    if(os.path.exists(path)):
+        os.unlink(path)
 
 def get_credentials(client_fd):
     '''Reads the peer credentials from a UDS descriptor'''
     if not hasattr(get_credentials, "SO_PEERCRED"):
         get_credentials.SO_PEERCRED = 17
     credentials = client_fd.getsockopt(SOL_SOCKET, \
-            get_credentials.SO_PEERCRED, struct.calcsize('3i'))
+                get_credentials.SO_PEERCRED, struct.calcsize('3i'))
     pid, uid, gid = struct.unpack('3i', credentials)
     return pid, uid, gid
 
+def stash_template():
+    '''Returns a fixed dictionary template to stash messages'''
+    if hasattr(stash_template, "stash_map"):
+        return common_utils.FixedDict(stash_template.stash_map)
+    stash_template.stash_map = {}
+    stash_template.stash_map['header'] = None # Header in bytes
+    stash_template.stash_map['header_object'] = None # Protobuf format
+    stash_template.stash_map['payload_len'] = 0
+    stash_template.stash_map['payload'] = None # Payload in bytes
+    stash_template.stash_map['payload_object'] = None # Protobuf format
+    return common_utils.FixedDict(stash_template.stash_map)
 
-def receive(sock_fd, size):
-    '''Received data for a given size from a socket'''
-    buf = ''
-    while size > 0:
-        data = sock_fd.recv(size)
-        if data == '':
-            break
-        buf += data
-        size -= len(data)
-    return buf
+def reset_stash(stash_map):
+    '''Resets the values for a given stash map'''
+    stash_map['header'] = None
+    stash_map['header_object'] = None
+    stash_map['payload_len'] = 0
+    stash_map['payload'] = None
+    stash_map['payload_object'] = None
 
 
-def read_data(sock_fd):
-    '''Receives data from a socket object and
-    returns a header and payload pair in bytes'''
-    # Read the header
-    hdr_buf = receive(sock_fd, common_utils.header_size())
-    if hdr_buf == '':
-        return None, None
-
-    header = uds_msg_pb2.Header()
-    header.ParseFromString(hdr_buf)
-    print "Header:", header.__str__()
-
-    # Get the payload size and type
-    payload_size, payload = common_utils.get_payload_type(header)
-
-    # Receive payload
-    payload_buf = receive(sock_fd, payload_size)
-    if payload_buf == '':
-        return hdr_buf, None
-
-    # Conversion to object only for debugging purposes
-    payload.ParseFromString(payload_buf)
-    print "Payload:", payload.__str__()
-
-    return hdr_buf, payload_buf
+def create_close_conn_obj(sock_fd):
+    '''Returns objects to mark a client connection close'''
+    print "Creating close message for", sock_fd.fileno()
+    return None, None
 
 
 def check_mailbox():
     '''Check for local messages'''
     pass
-
-
-def unlink(path):
-    '''Remove UDS link'''
-    if(os.path.exists(path)):
-        os.unlink(path)
 
 
 class CommunicationManager(object):
@@ -101,26 +85,29 @@ class CommunicationManager(object):
 
 class UDSCommunicationManager(CommunicationManager):
     '''UDS specific server implementation'''
+    StatusCode = common_utils.enum(success=0, \
+                close_connection=100, try_again_later=101)
     def __init__(self, path, max_conn=10, timeout=5.0):
         '''Initialize the class members'''
         super(UDSCommunicationManager, self).__init__()
-        unlink(path)
+        unlink_uds_path(path)
         self.input_fds = []
         self.uds_path = path # Configurable
         self.max_server_conn = max_conn # Configurable
         self.select_timeout = timeout # Configurable
         self.server_socket = None
+        self.msg_stash_map = {} # Map that holds a stash_info map per fd
 
         try:
             self.server_socket = \
                     socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
             self.server_socket.bind(self.uds_path)
             self.server_socket.listen(self.max_server_conn)
-        except socket.error as (err, msg):
+        except socket.error as err:
             if self.server_socket:
                 self.server_socket.close()
-            print "Error:", err, "Message:", msg
-            sys.exit(1)
+            logging.error("Error: %s", str(err))
+            raise common_utils.OPUSException("socket error")
         self.server_socket.setblocking(0) # Make the socket non-blocking
         self.input_fds = [self.server_socket]
 
@@ -129,51 +116,137 @@ class UDSCommunicationManager(CommunicationManager):
         ret_list = [] # List of tuples of form (header, payload)
 
         try:
-            input_ready, output_ready, except_ready = \
-                select.select(self.input_fds, [], [], self.select_timeout)
-        except (select.error, socket.error) as (err, msg):
-            print "Error:", err, "Message:", msg
+            input_ready, _, _ = \
+                    select.select(self.input_fds, [], [], self.select_timeout)
+        except (select.error, socket.error) as err:
+            logging.error("Error: %s", str(err))
             return ret_list
 
         if not input_ready:
-            print "select timed out"
+            logging.debug("select timed out")
             return ret_list
 
         for sock_fd in input_ready:
             if sock_fd == self.server_socket:
-                client_fd, address = self.server_socket.accept()
-                pid, uid, gid = get_credentials(client_fd)
-                print 'Got a new connection from pid: \
-                    %d, uid: %d, gid: %d' % (pid, uid, gid)
-                client_fd.setblocking(0) # Make the socket non-blocking
-                self.input_fds.append(client_fd) # Add it to the input fd list
+                self.__handle_new_connection()
             else:
-                header, payload = read_data(sock_fd)
-                if header or payload:
-                    print "Got data"
-                    ret_list.append((header, payload))
-                else:
-                    header, payload = create_close_conn_obj(sock_fd)
-                    print 'closing socket:', sock_fd.fileno()
-                    if sock_fd in self.input_fds:
-                        self.input_fds.remove(sock_fd)
-                    sock_fd.close()
+                self.__handle_client(sock_fd, ret_list)
+
         return ret_list
+
+
+    def __handle_client(self, sock_fd, ret_list):
+        '''Receives data from client or closes the client connection'''
+        status_code = self.__read_data(sock_fd)
+
+        if status_code == UDSCommunicationManager.StatusCode.success:
+            logging.debug("Got valid data")
+            ret_list.append((self.msg_stash_map[sock_fd]['header'], \
+                            self.msg_stash_map[sock_fd]['payload']))
+            reset_stash(self.msg_stash_map[sock_fd])
+        elif status_code == UDSCommunicationManager.StatusCode.close_connection:
+            ret_list.append(tuple(create_close_conn_obj(sock_fd)))
+            if sock_fd in self.input_fds:
+                self.input_fds.remove(sock_fd)
+            logging.debug('closing socket: %d', sock_fd.fileno())
+            sock_fd.close()
+            reset_stash(self.msg_stash_map[sock_fd])
+        elif status_code == UDSCommunicationManager.StatusCode.try_again_later:
+            logging.debug("Will try again later")
+
+    def __handle_new_connection(self):
+        '''Accepts and adds the new connection to the fd list'''
+        client_fd, _ = self.server_socket.accept()
+        pid, uid, gid = get_credentials(client_fd)
+        logging.debug("Got a new connection from pid: %d, uid: %d, gid: %d",
+                        pid, uid, gid)
+        client_fd.setblocking(0) # Make the socket non-blocking
+        self.input_fds.append(client_fd) # Add it to the input fd list
+        self.msg_stash_map[client_fd] = stash_template()
+
+
+
+    def __receive(self, sock_fd, size):
+        '''Received data for a given size from a socket'''
+        buf = ''
+        status_code = UDSCommunicationManager.StatusCode.success
+        while size > 0:
+            try:
+                data = sock_fd.recv(size)
+                if data == '':
+                    status_code = \
+                        UDSCommunicationManager.StatusCode.close_connection
+                    break
+            except socket.error as (err, msg):
+                if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                    status_code = \
+                        UDSCommunicationManager.StatusCode.try_again_later
+                elif err == errno.EINTR:
+                    logging.error("Error: %d, Message: %s", err, msg)
+                    continue
+                else:
+                    logging.error("Error: %d, Message: %s", err, msg)
+                    status_code = \
+                        UDSCommunicationManager.StatusCode.close_connection
+                break
+            buf += data
+            size -= len(data)
+        return buf, status_code
+
+    def __get_payload(self, sock_fd, stash_ref):
+        '''Receives the payload and stashes it'''
+        payload_buf, status_code = self.__receive(sock_fd, \
+                                    stash_ref['payload_len'])
+        if status_code != UDSCommunicationManager.StatusCode.success:
+            return status_code
+
+        payload = stash_ref['payload_object']
+        payload.ParseFromString(payload_buf)
+        logging.debug("Payload: %s", payload.__str__())
+        stash_ref['payload'] = payload_buf
+        stash_ref['payload_object'] = payload
+        return status_code
+
+    def __read_data(self, sock_fd):
+        '''Receives data from a socket object and
+        returns a header and payload pair in bytes'''
+        stash_ref = self.msg_stash_map[sock_fd] # Take a ref
+
+        # Read the header in not present
+        if not stash_ref['header']:
+            hdr_buf, status_code = self.__receive(sock_fd, \
+                                    common_utils.header_size())
+            if status_code != UDSCommunicationManager.StatusCode.success:
+                return status_code
+            stash_ref['header'] = hdr_buf
+            header = uds_msg_pb2.Header()
+            header.ParseFromString(hdr_buf)
+            stash_ref['header_object'] = header
+            logging.debug("Header: %s", header.__str__())
+
+            # Find out the payload length and type
+            payload_size, payload = common_utils.get_payload_type(header)
+            stash_ref['payload_object'] = payload
+            stash_ref['payload_len'] = payload_size
+            return self.__get_payload(sock_fd, stash_ref)
+
+        # We already have a header, now receive the payload
+        return self.__get_payload(sock_fd, stash_ref)
 
     def close(self):
         '''Close all connections and cleanup'''
         self.server_socket.close()
         for sock_fd in self.input_fds:
             sock_fd.close()
-        unlink(self.uds_path)
+        unlink_uds_path(self.uds_path)
 
 
 class Producer(threading.Thread):
     '''Base class for the producer thread'''
-    def __init__(self):
+    def __init__(self, analyser_obj):
         '''Initialize class data members'''
         super(Producer, self).__init__()
-        self.analyser = None
+        self.analyser = analyser_obj
         self.stop_event = threading.Event()
 
     def run(self):
@@ -183,7 +256,7 @@ class Producer(threading.Thread):
     def do_shutdown(self):
         '''Shutdown the thread gracefully'''
         self.analyser.do_shutdown()
-        print "Shutting down thread...."
+        logging.debug("Shutting down thread....")
         self.stop_event.set()
 
 
@@ -191,35 +264,51 @@ class SocketProducer(Producer):
     '''Implementation of a socket producer class'''
     def __init__(self, analyser_obj, comm_type):
         '''Initialize the class data members'''
-        super(SocketProducer, self).__init__()
+        super(SocketProducer, self).__init__(analyser_obj)
         self.comm_type = comm_type
-        #TODO: Change this to get from meta_factory
-        self.comm_manager = UDSCommunicationManager("./demo_socket", 5)
-        self.analyser = analyser_obj
+
+        try:
+            self.comm_manager = \
+                    common_utils.meta_factory(CommunicationManager, \
+                    "UDSCommunicationManager", "./demo_socket", 10, 5)
+        except common_utils.InvalidTagException as err_msg:
+            raise common_utils.OPUSException(err_msg.msg)
 
     def run(self):
         '''Spin until thread stop event is set'''
         while not self.stop_event.isSet():
             msg_list = self.comm_manager.do_poll()
             if not msg_list:
-                print "No message to be logged"
+                logging.debug("No message to be logged")
             else:
-                print "Calling put_msg on analyser"
-                self.analyser.put_msg(msg_list)
+                logging.debug("Calling put_msg on analyser")
+                self.__send_data_to_analyser(msg_list)
             check_mailbox()
         self.comm_manager.close()
+
+    @common_utils.analyser_lock
+    def __send_data_to_analyser(self, msg_list):
+        '''Calls the analyser object method by obtaining a lock'''
+        self.analyser.put_msg(msg_list)
 
     def do_shutdown(self):
         '''Shutdown the thread gracefully'''
         super(SocketProducer, self).do_shutdown()
-        #self.comm_manager.close()
 
 # Uncomment for testing purposes
 #if __name__ == "__main__":
-#    analyser_object = analysis.LoggingAnalyser("prov_log.dat")
-#    producer_object = SocketProducer(analyser_object, "UDSCommunicationManager")
-#    producer_object.start()
-#    time.sleep(20)
-#    producer_object.do_shutdown()
-#    producer_object.join()
-#    print "Exiting master thread"
+#    logging.basicConfig(format='%(asctime)s %(message)s', \
+#                        datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
+#    try:
+#        analyser_object = analysis.LoggingAnalyser("prov_log.dat")
+#        producer_object = SocketProducer(analyser_object, \
+#                            "UDSCommunicationManager")
+#   except common_utils.OPUSException as message:
+#        logging.error(message)
+#        sys.exit(1) # Depends on how the DaemonManager handles this
+#
+#   producer_object.start()
+#   time.sleep(20)
+#   producer_object.do_shutdown()
+#   producer_object.join()
+#   logging.debug("Exiting master thread")
