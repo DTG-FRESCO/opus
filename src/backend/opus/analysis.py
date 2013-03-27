84341f59c58f2c102134f9e00457c53ae7d8a092
Nikilesh Balakrishnan <nb466@cl.cam.ac.uk>
2013-03-27 17:19:45 +0000

    changes for future imports

--- src/backend/opus/analysis.py
+++ src/backend/opus/analysis.py
@@ -4,6 +4,9 @@ provenance data sent by the producer. Various
 types of analysers have been defined in this file.
 '''
 
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
 import os
 import logging
 import threading
