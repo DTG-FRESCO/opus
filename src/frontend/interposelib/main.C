9c36cf60f2c05e00382eb4983c59534ddcb213ff
Nikilesh Balakrishnan <nb466@cl.cam.ac.uk>
2013-04-17 17:24:03 +0100

    Fixed heisenbugvim main.C Initialized char buffers to null, to avoid protocol buffer serialization errors

--- src/frontend/interposelib/main.C
+++ src/frontend/interposelib/main.C
@@ -14,6 +14,9 @@ void send_startup_message(){
     char link[1024];
     char exe[1024];
 
+    memset(link, 0, sizeof(link));
+    memset(exe, 0, sizeof(exe));
+
     snprintf(link,sizeof(link),"/proc/%d/exe",getpid());
     if (readlink(link,exe,sizeof(exe)) >= 0) {
         start_msg.set_exec_name(exe);
@@ -56,7 +59,7 @@ void deinitialise(){
 void initialise(){
     ProcUtils::test_and_set_flag(true);
 
-    if (UDSCommClient::get_instance()->connect("/auto/homes/nb466/opus/src/backend/demo_socket")){
+    if (UDSCommClient::get_instance()->connect("./demo_socket")){
         send_startup_message();
         ProcUtils::test_and_set_flag(false);
     }
