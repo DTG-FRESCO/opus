1fcc8dfec9b3ba6895c1970b5f2e5270a073eeea
Thomas Bytheway <tb403@cam.ac.uk>
2013-04-23 14:04:11 +0100

    Reorganisation of the structure of the interposition system.
    functions.C and functions.h are now static files that aggregate
    other interposition files and store common code.
    gen_boiler now produces gen_functions.C.
    
    Signed-off-by: Thomas Bytheway <tb403@cam.ac.uk>

--- src/frontend/interposelib/functions.h
+++ src/frontend/interposelib/functions.h
@@ -1,8 +1,9 @@
-{% import 'macros.tmpl' as macros -%}
 #ifndef SRC_FRONTEND_GEN_BOILER_FUNCTIONS_H_
 #define SRC_FRONTEND_GEN_BOILER_FUNCTIONS_H_
 
 #include "protobuf_typedefs.h"
+
+#define NUM_BUFF_SIZE 50
 
 #define DLSYM_CHECK(A) if ((A)==NULL){ \
   DEBUG_LOG("[%s:%d]: %s\n", __FILE__, __LINE__, "Critical error, failed to dlsym a function correctly."); \
@@ -13,4 +14,3 @@
 }
 
 #endif  // SRC_FRONTEND_GEN_BOILER_FUNCTIONS_H_
-
