d76a6b28f40de568e5bfceabc2b011ec26ed274f
Nikilesh Balakrishnan <nb466@cl.cam.ac.uk>
2013-04-23 13:40:59 +0100

    Moved protobuf typedefs under a single header. Moved the dlsym check to functions.h

---
+++ src/frontend/interposelib/protobuf_typedefs.h
@@ -0,0 +1,15 @@
+#ifndef SRC_FRONTEND_INTERPOSELIB_PROTOBUF_TYPEDEFS_H_
+#define SRC_FRONTEND_INTERPOSELIB_PROTOBUF_TYPEDEFS_H_
+
+#include "uds_msg.pb.h"
+
+typedef ::google::protobuf::Message Message;
+typedef ::fresco::opus::IPCMessage::KVPair KVPair;
+typedef ::fresco::opus::IPCMessage::StartupMessage StartupMessage;
+typedef ::fresco::opus::IPCMessage::FuncInfoMessage FuncInfoMessage;
+typedef ::fresco::opus::IPCMessage::Header HeaderMessage;
+typedef ::fresco::opus::IPCMessage::LibInfoMessage LibInfoMessage;
+typedef ::fresco::opus::IPCMessage::GenericMessage GenericMessage;
+typedef ::fresco::opus::IPCMessage::PayloadType PayloadType;
+
+#endif  // SRC_FRONTEND_INTERPOSELIB_PROTOBUF_TYPEDEFS_H_
