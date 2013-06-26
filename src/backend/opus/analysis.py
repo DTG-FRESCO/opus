a8f6d5f1bd5c37214bf562026cd984459e0c36b9
Thomas Bytheway <tb403@cam.ac.uk>
2013-06-26 17:37:26 +0100

    Implemented a real-time PVM Analyser.
    Currently loosely tied to an implementation of PVM for POSIX.
    Currently only using a naive implementation of rename awaiting
    future refactoring.
    Does not implement *at operations awaiting further planning.
    
    Signed-off-by: Thomas Bytheway <tb403@cam.ac.uk>

--- src/backend/opus/analysis.py
+++ src/backend/opus/analysis.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 This module performs the analysis of incoming
 provenance data sent by the producer. Various
@@ -7,17 +8,23 @@ types of analysers have been defined in this file.
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
+import Queue
 import os
 import logging
 import threading
+import time
 
-from opus import common_utils, uds_msg_pb2
+
+from opus import common_utils, storage, order
+from opus import uds_msg_pb2 as uds_msg
+from opus.pvm import posix
+
 
 class Analyser(threading.Thread):
     '''Base class for the analyser'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Initialize class members'''
-        super(Analyser, self).__init__()
+        super(Analyser, self).__init__(*args, **kwargs)
         self.stop_event = threading.Event()
 
     def run(self):
@@ -42,15 +49,16 @@ class Analyser(threading.Thread):
 
 
 def create_blank_marker():
+    '''Create a blank message to be inserted as a break into the log.'''
     logging.debug("Creating blank message.")
     if hasattr(create_blank_marker, "blank_msg"):
         return create_blank_marker.blank_msg
 
-    header = uds_msg_pb2.Header()
+    header = uds_msg.Header()
     header.timestamp = 0
     header.pid = 0
     header.tid = 0
-    header.payload_type = uds_msg_pb2.BLANK_MSG
+    header.payload_type = uds_msg.BLANK_MSG
     header.payload_len = 0
 
     create_blank_marker.blank_msg = header.SerializeToString()
@@ -60,9 +68,9 @@ def create_blank_marker():
 
 class LoggingAnalyser(Analyser):
     '''Implementation of a logging analyser'''
-    def __init__(self, log_path):
+    def __init__(self, log_path, *args, **kwargs):
         '''Initialize class members'''
-        super(LoggingAnalyser, self).__init__()
+        super(LoggingAnalyser, self).__init__(*args, **kwargs)
         self.file_object = None
         self.logfile_path = log_path
 
@@ -90,3 +98,113 @@ class LoggingAnalyser(Analyser):
         and close the file object'''
         os.fsync(self.file_object.fileno())
         self.file_object.close()
+
+
+class OrderingAnalyser(Analyser):
+    '''The ordering analyser implements a event ordering queue and calls the
+    process method to consume messages.'''
+    def __init__(self, *args, **kwargs):
+        super(OrderingAnalyser, self).__init__(*args, **kwargs)
+        self.event_orderer = order.EventOrderer(50) #TODO - Proper max_wind
+        self.queue_cleared = threading.Event()
+
+    def run(self):
+        '''Pull events from the queue and process them as long as the stop_event
+        is not set.'''
+        while not self.stop_event.is_set():
+            try:
+                _, msg = self.event_orderer.pop()
+                self.process(msg)
+            except Queue.Empty:
+                logging.debug("T:Queue cleared, clearing state tables.")
+                self.queue_cleared.set()
+                self.cleanup()
+                logging.debug("T:Preparing to wait for clear completion.")
+                while self.queue_cleared.is_set():
+                    time.sleep(0)
+                logging.debug("T:Clear completed.")
+                continue
+
+    def do_shutdown(self):
+        '''Clear the event orderer and then shutdown the processing thread.'''
+        logging.debug("M:Shutting down analyser.")
+        logging.debug("M:Starting queue flush.")
+        self.event_orderer.start_clear()
+        logging.debug("M:Waiting for queue flush completion.")
+        self.queue_cleared.wait()
+        logging.debug("M:Stopping thread.")
+        self.stop_event.set()
+        logging.debug("M:Completing flush.")
+        self.queue_cleared.clear()
+        super(OrderingAnalyser, self).do_shutdown()
+
+    def put_msg(self, msg_list):
+        '''Place a set of messages onto the queue, clearing the queue if any
+        blank markers are found.'''
+        msg_chunk = []
+        for hdr, pay in msg_list:
+            hdr_obj = uds_msg.Header()
+            hdr_obj.ParseFromString(hdr)
+            pay_obj = common_utils.get_payload_type(hdr_obj)
+            pay_obj.ParseFromString(pay)
+
+            if hdr_obj.payload_type == 0:
+                logging.debug("M:Received blank message.")
+                logging.debug("M:Pushing remaining message chunk.")
+                logging.debug("M:Message chunk length:%d", len(msg_chunk))
+                self.event_orderer.push(msg_chunk)
+                logging.debug("M:Signalling queue clear.")
+                self.event_orderer.start_clear()
+                logging.debug("M:Waiting for completion.")
+                self.queue_cleared.wait()
+                logging.debug("M:Stopping clear.")
+                self.event_orderer.stop_clear()
+                logging.debug("M:Signalling clear completed.")
+                self.queue_cleared.clear()
+                logging.debug("M:Queue cleared, continuing.")
+            else:
+                msg_chunk += [(hdr_obj.timestamp, (hdr_obj, pay_obj))]
+        self.event_orderer.push(msg_chunk)
+
+    def cleanup(self):
+        '''Run any code that needs to happen whenever the queue is cleared.'''
+        raise NotImplementedError()
+
+    def process(self, (hdr, pay)):
+        '''Process a single message.'''
+        raise NotImplementedError()
+
+
+class PVMAnalyser(OrderingAnalyser):
+    '''The PVM analyser class implements the core of the PVM model, including
+    the significant operations and their interactions with the underlying
+    storage system.'''
+    def __init__(self, storage_args, *args, **kwargs):
+        super(PVMAnalyser, self).__init__(*args, **kwargs)
+        self.storage_interface = storage.StorageIFace(**storage_args)
+        
+    def run(self):
+        '''Run a standard processing loop, also close the storage interface once
+        it is complete.'''
+        super(PVMAnalyser, self).run()
+        self.storage_interface.close()
+
+    def cleanup(self):
+        '''Clear the process data structures.'''
+        posix.PIDMAP.clear()
+        posix.DisconController.clear()
+
+    def process(self, (hdr, pay)):
+        '''Process a single front end message, applying it's effects to the
+        database.'''
+        tran = self.storage_interface.start_transaction()
+        if hdr.payload_type == uds_msg.FUNCINFO_MSG:
+            posix.handle_function(tran, hdr.pid, pay)
+        elif hdr.payload_type == uds_msg.STARTUP_MSG:
+            posix.handle_process(tran, hdr, pay)
+        elif hdr.payload_type == uds_msg.GENERIC_MSG:
+            if pay.msg_type == uds_msg.DISCON:
+                posix.handle_disconnect(hdr.pid)
+            elif pay.msg_type == uds_msg.PRE_FUNC_CALL:
+                posix.handle_prefunc(hdr.pid, pay)
+        tran.commit()
---
+++ src/backend/opus/pvm/posix/actions.py
@@ -0,0 +1,189 @@
+# -*- coding: utf-8 -*-
+'''
+Implementations of many standard actions that posix functions can perform that
+are shown in terms of their PVM semantics.
+'''
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+
+import logging
+
+from opus import pvm
+from opus.pvm.posix import utils
+from opus import prov_db_pb2 as prov_db
+
+
+class ActionMap(object):
+    '''Mapping of action names to action functions.'''
+    action_map = {}
+
+    @classmethod
+    def get(cls, key):
+        '''Return the function associated with key.'''
+        return cls.action_map[key]
+
+    @classmethod
+    def add(cls, name):
+        '''Adds name => fun to the map.'''
+        def wrapper(fun):
+            '''Decorator internal.'''
+            cls.action_map[name] = fun
+            return fun
+        return wrapper
+
+
+@ActionMap.add('event')
+def event_action(*args):
+    '''Action representing a function that only generates a process event.'''
+    return args[1] #p_id
+
+
+@ActionMap.add('read')
+def read_action(tran, p_id, msg, fd_arg):
+    '''Action that reads from a names file descriptor.'''
+    return null_action(tran, p_id, msg, fd_arg)
+
+
+@ActionMap.add('write')
+def write_action(tran, p_id, msg, fd_arg):
+    '''Action that writes to a named file descriptor.'''
+    return null_action(tran, p_id, msg, fd_arg)
+
+
+@ActionMap.add('null')
+def null_action(tran, p_id, msg, fd_arg):
+    '''Action that interacts with a file descriptor but is neither a read or a
+    write. Also the common basis between both the read and write actions.'''
+    if isinstance(fd_arg, unicode) or isinstance(fd_arg, str):
+        args = utils.parse_kvpair_list(msg.args)
+        file_des = args[fd_arg]
+    else:
+        file_des = fd_arg
+    l_id = utils.proc_get_local(tran, p_id, file_des)
+    return l_id
+
+
+@ActionMap.add('open')
+@utils.check_message_error_num
+def open_action(tran, p_id, msg, file_arg):
+    '''Action that opens a named file.'''
+    args = utils.parse_kvpair_list(msg.args)
+    l_id = pvm.get_l(tran, p_id, str(msg.ret_val))
+    pvm.get_g(tran, l_id, args[file_arg])
+    return l_id
+
+
+@ActionMap.add('close')
+def close_action(tran, p_id, msg, fd_arg):
+    '''Action that closes a named file descriptor.'''
+    args = utils.parse_kvpair_list(msg.args)
+    try:
+        l_id = utils.proc_get_local(tran, p_id, args[fd_arg])
+    except utils.NoMatchingLocalError:
+        return p_id
+
+    if msg.error_num > 0:
+        return l_id
+    l_obj = tran.get(l_id)
+    if len(l_obj.file_object) > 0:
+        if len(l_obj.file_object) > 1:
+            logging.error("Closing invalid local object.")
+        g_id = l_obj.file_object[0].id
+        pvm.drop_g(tran, l_id, g_id)
+        l_obj = tran.get(l_id)
+        new_l_id = l_obj.next_version.id
+        pvm.drop_l(tran, new_l_id)
+    else:
+        pvm.drop_l(tran, l_id)
+
+    return l_id
+
+
+def delete_single_name(tran, omega_id, g_id):
+    '''Deletes a file with a single name.'''
+    g_obj = tran.get(g_id)
+    new_g_id = pvm.drop_g(tran, omega_id, g_id)
+    new_g_obj = tran.get(new_g_id)
+    new_g_obj.prev_version[0].state = prov_db.DELETED
+    g_obj.next_version[0].state = prov_db.DELETED
+    for lnk in new_g_obj.process_object:
+        pvm.unbind(tran, lnk.id, new_g_id)
+
+
+def delete_multiple_names(tran, omega_id, g_id, g_name):
+    '''Deletes a file with multiple names.'''
+    g_obj = tran.get(g_id)
+    main_g_id = pvm.drop_g(tran, omega_id, g_id)
+    main_g_obj = tran.get(main_g_id)
+    for i in range(len(main_g_obj.name)):
+        if main_g_obj.name[i] == g_name:
+            del main_g_obj.name[i]
+            break
+    for lnk in main_g_obj.process_object:
+        loc = tran.get(lnk.id)
+        loc.ref_count = loc.ref_count - 1
+    (side_g_id, side_g_obj) = tran.create(prov_db.GLOBAL)
+    side_g_obj.name.append(g_name)
+    tran.name_put(g_name, side_g_id)
+    fwd = g_obj.next_version.add()
+    fwd.id = side_g_id
+    fwd.state = prov_db.DELETED
+    bck = side_g_obj.prev_version.add()
+    bck.id = g_id
+    bck.state = prov_db.DELETED
+
+
+@ActionMap.add('delete')
+@utils.check_message_error_num
+def delete_action(tran, p_id, msg, file_arg):
+    '''Action that deletes a named file.'''
+    args = utils.parse_kvpair_list(msg.args)
+    l_id = pvm.get_l(tran, p_id, "omega")
+    g_name = args[file_arg]
+    g_id = pvm.get_g(tran, l_id, g_name)
+    g_obj = tran.get(g_id)
+    if len(g_obj.name) == 1:
+        delete_single_name(tran, l_id, g_id)
+    else:
+        delete_multiple_names(tran, l_id, g_id, g_name)
+    new_l_id = tran.get(l_id).next_version.id
+    pvm.drop_l(tran, new_l_id)
+    return l_id
+
+
+@ActionMap.add('link')
+@utils.check_message_error_num
+def link_action(tran, p_id, msg, orig_arg, new_arg):
+    '''Action that links a new name to an existing file.'''
+    args = utils.parse_kvpair_list(msg.args)
+    l_id = pvm.get_l(tran, p_id, "omega")
+    orig_id = pvm.get_g(tran, l_id, args[orig_arg])
+    new_id = pvm.get_g(tran, l_id, args[new_arg])
+    orig_id = pvm.drop_g(tran, l_id, orig_id)
+    new_obj = tran.get(new_id)
+    for lnk in new_obj.process_object:
+        if lnk.id != l_id:
+            pvm.version_local(tran, lnk.id, orig_id)
+    orig_obj = tran.get(orig_id)
+    for name in new_obj.name:
+        orig_obj.name.append(name)
+    new_obj.next_version.add().id = orig_id
+    orig_obj.prev_version.add().id = new_id
+    new_l_id = tran.get(l_id).next_version.id
+    pvm.drop_l(tran, new_l_id)
+    return l_id
+
+@ActionMap.add('touch')
+@utils.check_message_error_num
+def touch_action(tran, p_id, msg, file_arg):
+    '''Action that touches a named file.'''
+    args = utils.parse_kvpair_list(msg.args)
+    l_id = pvm.get_l(tran, p_id, "omega")
+    g_id = pvm.get_g(tran, l_id, args[file_arg])
+    pvm.drop_g(tran, l_id, g_id)
+    l_obj = tran.get(l_id)
+    new_l_id = l_obj.next_version.id
+    pvm.drop_l(tran, new_l_id)
+    return l_id
