f05138ce3c29840f5c080aa6c04f7e2ebe970562
Nikilesh Balakrishnan <nb466@cl.cam.ac.uk>
2013-08-16 10:27:03 +0100

    Added documentation for the errno tracking class
    Signed-off-by: Nikilesh Balakrishnan <nb466@cl.cam.ac.uk>

--- src/frontend/interposelib/track_errno.h
+++ src/frontend/interposelib/track_errno.h
@@ -3,24 +3,42 @@
 
 #include <errno.h>
 
+/**
+ * RAII class to track and restore the correct
+ * value of errno within a glibc wrapper function.
+ */
 class TrackErrno
 {
     public:
+        /**
+         * ctor strores the current errno value
+         */
         TrackErrno(const int err_val)
         {
             saved_errno = err_val;
         }
 
+        /**
+         * dtor restores the saved errno value
+         */
         ~TrackErrno()
         {
             errno = saved_errno;
         }
 
+        /**
+         * Option to update the class object
+         * with a new errno value.
+         */
         void operator=(const int err_val)
         {
             saved_errno = err_val;
         }
 
+        /**
+         * Can be used if the error object needs
+         * to be assigned to an integer variable.
+         */
         operator int() const
         {
             return saved_errno;
